74789fd47241feb501cbfad66421809817f729a6
Chris Sciberas <55458856+cscibera@users.noreply.github.com>
2021-09-27 10:18:25 -0400

    Topic Modeling DF-Test
    Code used to test Topic Modeling DF

---
+++ R/Topic Modeling DF-Test.R
@@ -0,0 +1,9 @@
+#This article does appear in the Df and demonstrates that the code works 
+sum(df_article_communities$title == "Authoritarianism of Parents and Offspring: Intergenerational Politics and Adjustment to College")
+
+## The remaining articles do not appear in DF
+sum(df_article_communities$title == "Construction of circumplex scales for the Inventory of Interpersonal Problems")
+sum(df_article_communities$title == "Personality trait structure as a human universal")
+sum(df_article_communities$title == "Evaluating Stability and Change in Personality and Depression")
+sum(df_article_communities$title == "On the accuracy of personality judgment: A realistic approach")
+sum(df_article_communities$title == "The divided self: Concurrent and longitudinal effects of psychological adjustment and social roles on self-concept differentiation")
