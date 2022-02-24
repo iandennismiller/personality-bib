4592ff34855d5b9ab7c61f0f568459ec776e2969
Ian Dennis Miller <ian@iandennismiller.com>
2022-02-23 22:21:52 -0500

    re-think R to get titles and communities together

--- R/utils/coauthorship-graph-helper-functions.R
+++ R/utils/coauthorship-graph-helper-functions.R
@@ -1,4 +1,9 @@
 library(igraph)
+library(dplyr)
+
+find_author = function(graph, name) {
+  return(V(graph)[V(graph)$node_type == "author" & V(graph)$author_name == name])
+}
 
 get_authors_df = function(journal_graph) {
   # obtain all vertices from graph as data frame
@@ -41,16 +46,60 @@ get_main_component = function(journal_graph) {
   return(main_component_graph)
 }
 
-get_article_communities = function(journal_graph, main_component_graph) {
-  tmp_data_frame = get.data.frame(main_component_graph, what="vertices")
+get_authors_df_from_graph = function(graph) {
+  tmp_data_frame = get.data.frame(graph, what="vertices")
   df_authors = tmp_data_frame[which(tmp_data_frame$node_type == "author"),]
   df_authors = df_authors[c('id', 'author_name', 'scopus_id', 'membership')]
+  return(df_authors)
+}
 
+get_articles_df_from_graph = function(graph) {
+  tmp_data_frame = get.data.frame(graph, what="vertices")
+  df_authors = tmp_data_frame[which(tmp_data_frame$node_type == "article"),]
+  df_authors = df_authors[c('id', 'author_name', 'scopus_id', 'membership')]
+  return(df_authors)
+}
+
+get_article_communities = function(journal_graph, main_component_graph) {
+  df_main_component = get.data.frame(main_component_graph, what="vertices")
+
+  df_authors_articles = get.data.frame(journal_graph, what="edges")
+  df_authors_articles = df_authors_articles[which(df_authors_articles$edge_type == "authored"),]
+  df_authors_articles$from = paste0("n", df_authors_articles$from)
+  df_authors_articles$to = paste0("n", df_authors_articles$to)
+
+  df_all_authors = get.data.frame(journal_graph, what="vertices")
+  df_all_authors = df_all_authors[which(df_all_authors$node_type == "author"),]
+
+  df_all_articles = get.data.frame(journal_graph, what="vertices")
+  df_all_articles = df_all_articles[which(df_all_articles$node_type == "article"),]
+
+  df_all = df_authors_articles %>%
+    left_join(df_all_authors, by=c("from"="id")) %>%
+    left_join(df_all_articles, by=c("to"="id"))
+  df_all = df_all[c('author_name.x', 'scopus_id.x', 'doi.y', 'title.y')]
+  names(df_all) = c("author_name", "scopus_id", "doi", "title")
+
+  df_all_membership = df_all %>%
+    left_join(df_main_component, by=c("scopus_id" = "scopus_id"))
+
+  return(df_all)
+}
+
+# create data frame with every article title, along with the community the first author belongs to
+get_article_communities_graph = function(journal_graph, main_component_graph) {
+
+  # we start with all authors in the main component
+  df_authors = get_authors_df_from_graph(main_component_graph)
+
+  # we will build a new data frame with the article titles
   df_article_communities = data.frame(
     id=character(),
     title=character(),
     community=integer()
   )
+
+  # go one article at a time through the journal graph
 
   for (scopus_id_str in df_authors$scopus_id) {
     # obtain an author vertex based on Scopus ID
